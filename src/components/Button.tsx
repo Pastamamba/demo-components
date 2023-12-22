95dedbf83a6536bf2af6de8de363336c5eddc0da
Petri <35950897+Pastamamba@users.noreply.github.com>
2023-12-22 20:01:26 +0200

    Button fixed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({ isActive, isDisabled, direction }) => {
     const baseStyles = 'flex items-center justify-center transition duration-150 ease-in-out m-2 text-2xl';
     const activeStyles = isActive ? 'bg-fireRed-2' : 'bg-fireRed-1';
-    const disabledStyles = isDisabled ? 'opacity-50 cursor-not-allowed text-oxfordBlue-4 bg-oxfordBlue-6' : '';
+    const disabledStyles = isDisabled ? 'opacity-50 cursor-not-allowed text-gray-400 bg-gray-600' : '';
     const hoverStyles = !isDisabled ? 'hover:bg-fireRed-2' : '';
     const sizeStyles = 'w-12 h-12';
     const roundedStyles = 'rounded-full';
@@ -17,14 +17,28 @@ const Button: React.FC<ButtonProps> = ({ isActive, isDisabled, direction }) => {
 
     const arrowStyles = 'flex justify-center items-center';
 
-    const arrowIcon = direction === 'left' ? '<' : '>';
+    const LeftArrowSvg = (
+        <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <polyline points="15 18 9 12 15 6"></polyline>
+        </svg>
+    );
+
+    const RightArrowSvg = (
+        <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <polyline points="9 18 15 12 9 6"></polyline>
+        </svg>
+    );
+
+    const arrowIcon = direction === 'left' ? LeftArrowSvg : RightArrowSvg;
 
     return (
         <button
             className={`${baseStyles} ${isDisabled ? disabledStyles : `${activeStyles} ${hoverStyles}`} ${sizeStyles} ${roundedStyles} ${fontStyles}`}
             disabled={isDisabled}
         >
-            <span className={arrowStyles}>{arrowIcon}</span>
+      <span className={arrowStyles}>
+        {arrowIcon}
+      </span>
         </button>
     );
 };
